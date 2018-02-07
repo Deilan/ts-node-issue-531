de8a02a61e434595640f0ae8f9868e1393d63b89
Deilan <deilan@mail.ru>
2018-02-07 14:52:31 +0300

    upgrade nodemon and ts-node packages, implement cross-env, housekeeping

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Greeter } from "./greeter";
 
-const greeter = new Greeter<string>("Hello, world");
+const greeter = new Greeter<string>(process.env.NODE_ENV!);
 
 setTimeout(() => {
     // tslint:disable-next-line:no-console
     console.log(greeter.greet());
-}, 5000);
+}, 10000);
