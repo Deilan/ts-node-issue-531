f4a542bfd83756b0ffc494590e112db8a95f47bc
Deilan <deilan@mail.ru>
2018-02-09 16:55:09 +0300

    prepare for https://github.com/TypeStrong/ts-node/issues/531

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Greeter } from "./greeter";
 
 const greeter = new Greeter<string>(process.env.NODE_ENV!);
 
-setTimeout(() => {
+setInterval(() => {
     // tslint:disable-next-line:no-console
     console.log(greeter.greet());
-}, 10000);
+}, 1000);
